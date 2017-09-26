1f30e2a27ea515ab91fab5e52f5f11f136bf4396
jonjperkins <jjperk@gmail.com>
2017-09-26 10:23:02 -0500

    Add functions and endpoint for TrustedForm resubmission tool

--- server.js
+++ server.js
@@ -13,28 +13,130 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
+var errorArray = [];
+var responseArray = [];
+var encoded_api_key = "";
+var tf_account_name = "";
+
+function getErrors(req, callback) {
+  var api_key = req.body.api_key;
+    base_64_encoded = new Buffer('tfclaimtool' + ":" + api_key).toString('base64');
+    var options = {
+      url: req.body.URL,
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Basic ' + base_64_encoded
+      }
+    }
+    encoded_api_key = base_64_encoded  
+   request(options, function (error, response, body) {
+      var lead_id = [];
+      var trustedform_cert_url = [];
+      var reason = [];
+      var timestamp = [];
+      var account = [];
+      var body = [];
+      var tf_object_array = [];
+      var empty_array = [];
+      if (error) {
+        callback(error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        callback(response.statusCode);
+        console.log("here? " + response.statusCode)
+        return;
+      }
+      var json_object = JSON.parse(response.body)
+      if (json_object.length > 1) {
+        var tf_account_name = json_object[0].vars.account.name;
+      }
+      
+      if (json_object.length > 0) {
+        for (var i = 0; i < json_object.length; i++) {
+          lead_id.push(json_object[i].vars.lead.id);
+          trustedform_cert_url.push("https://cert.trustedform.com" + json_object[i].vars.lead.trustedform_cert_url.path);
+          reason.push(json_object[i].reason);
+          timestamp.push(json_object[i].vars.submission.timestamp);
+          account.push(json_object[i].vars.account.name);
+          body.push(json_object[i].transactions[0].request.body)
+        };
+        for (var i = 0; i < trustedform_cert_url.length; i++) {
+          var tf_object = {};
+          tf_object['lead_id'] = lead_id[i]
+          tf_object['trustedform_cert_url'] = trustedform_cert_url[i];
+          tf_object['reason'] = reason[i];
+          tf_object['timestamp'] = timestamp[i];
+          tf_object['account'] = account[i];
+          tf_object['body'] = body[i];
+          tf_object_array.push(tf_object)
+        }
+        errorArray = tf_object_array;
+        account = tf_object_array[0].account;
+        callback(null, tf_object_array);
+        console.log('NOT empty array')
+        return;
+      } else {
+        errorArray = empty_array;
+        console.log(typeof empty_array)
+        console.log('empty array')
+        callback();
+        return;
+      }
+  });  
+}
+
+function resubmitCertificateArray(certArray, callback) {
+  var length = certArray.length;
+  var responses = [];
+  console.log('initial length: ' + certArray.length)
+
+    if (length > 0) {
+        for (var i=0; i < (length); i++) {
+            var json_post_body = qs.parse("api_key_apis=" + encoded_api_key + "&" + "campaign_id=" + certArray[i].account + "&" + "trustedform_cert_url=" + certArray[i].trustedform_cert_url + "&" + certArray[i].body);
+            var options = {
+                url: "https://app.leadconduit.com/flows/59aec194803f022c4f000000/sources/59aec2571486a71874fb82ac/submit",
+                headers: {
+                    "accept": "application/json",
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(json_post_body)
+            }
+            request.post(options, function (error, response, body) {
+                var response_object = JSON.parse(response.body)
+                responses.push(response.body);
+                if (responses.length === length) {
+                  responses.push(certArray[0].account)
+                  callback(null, responses);
+                  return;
+                }
+            })
+        }
+    }
+}
+
 
 app.post('/', function(req, res, next) {
-	//var webpage_html = ''
-	//var fields = ''
-	//var webpage_regex = /<input.*name="(.*?)"/g;
+  //var webpage_html = ''
+  //var fields = ''
+  //var webpage_regex = /<input.*name="(.*?)"/g;
     console.log("Webhook received!");
     console.log("Url to GET: " + req.body.get_url)
     var request_url = req.body.get_url;
     var username = req.body.username;
     var api_key = req.body.api_key;
 
-	request(request_url, function (error, response, body) {
-  		console.log('body:', body);
-  		var fields = []
-  		var regex = /<input[\s\S]*?name="(.*?)"/g
-		var item
-		while (item = regex.exec(body))
-			fields.push(item[1]);
+  request(request_url, function (error, response, body) {
+      console.log('body:', body);
+      var fields = []
+      var regex = /<input[\s\S]*?name="(.*?)"/g
+    var item
+    while (item = regex.exec(body))
+      fields.push(item[1]);
       fields = fields.join(',');
-  		res.send(fields);
-	});
-	
+      res.send(fields);
+  });
+  
 });
 
 app.post('/test-tool', function(req, res, next) {
@@ -102,19 +204,42 @@ app.post('/flow-name', function(req, res, next) {
     });
 })
 
+app.post('/trusted-form-errors', function(req, res, next) {
+    getErrors(req, function (err, data) {
+      console.log('what data looks like: ' + err);
+      if (err === 401) {
+        res.write("bad_api_key");
+        res.end();
+        return;
+      } else if (data === undefined) {
+          res.write("none");
+          res.end();
+          return;
+      } else if (err) {
+        res.write("unknown_error")
+        res.end();
+        return;
+      } else {
+        resubmitCertificateArray(data, function (err, data) {
+          if (err) {
+            console.log('401 here: ' + err)
+            // res.write(err);
+            // res.end();
+            res.end();
+            return;
+          } else {
+            console.log('resubmit data: ' + data.toString());
+            res.send(data);
+            res.end();
+            return;
+          }
+        });
+        
+      }
+    });
+})
+
 
 app.listen(PORT, function () {
-  	console.log('Listening on port 8080!');
+    console.log('Listening');
 });
-
-/* var request = new Request("https://next.leadconduit.com/flows/" + flow_id + "/fields", {
-        method: 'GET', 
-        headers: new Headers({
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Accept': 'application/vnd.com.leadconduit.field+json',
-          'Authorization': 'Basic ' + base_64_encode
-        })
-      });
-*/
-
-
